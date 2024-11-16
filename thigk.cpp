f9cc0a0c2d8098c4c6416fd1a6a540aab28dc5c8
Tempest <hoangcong25022003@gmail.com>
2024-11-16 08:05:03 +0700

    kiem tra le

--- thigk.cpp
+++ thigk.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
+
+void ktraLe(int n){
+	if(n%2!=0)
+		cout<<n<<" la so le"<<endl;
+	else
+		cout<<n<<" khong la so le"<<endl;
+}
+
 void main(){
 	int n;
 	cout<<"Nhap vao mot so nguyen: ";
 	cin>>n;
+	ktraLe(n);
 }
