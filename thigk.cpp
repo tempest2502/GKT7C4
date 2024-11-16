10c95e04ef5f8eb6d808c0e840780c9837479793
Tempest <hoangcong25022003@gmail.com>
2024-11-16 07:59:53 +0700

    kiem tra chan

--- thigk.cpp
+++ thigk.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
+
+void ktraChan(int n){
+	if(n%2==0)
+		cout<<n<<" la so chan"<<endl;
+	else
+		cout<<n<<" khong la so chan"<<endl;
+}
+
 void main(){
 	int n;
 	cout<<"Nhap vao mot so nguyen: ";
 	cin>>n;
+	ktraChan(n);
 }
